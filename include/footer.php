e023ee07f2fdaeb124144e08ad41bdced2bda7b1
amranhakim10 <amranhakim9@gmail.com>
2023-01-28 12:12:48 +0700

    Initial commit

--- include/footer.php
+++ include/footer.php
@@ -1,73 +1,51 @@
-<footer class="pb_footer " role="contentinfo" style="background-color: #f7b600;">
+<footer class="footer_area section_gap">
     <div class="container">
-        <div class="row" style="display: flex;justify-content: space-between;align-content: center;">
-            <div class="col-sm-6">
-                <div>
-                    <p style="color: #fff;text-align: justify;line-height: 25px">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor magna nulla, vel mattis augue laoreet non. Donec sed fermentum dolor, in pharetra elit. Integer sit amet facilisis nulla, sit amet porta elit. Sed dapibus urna congue
-                    </p>
-                </div>
-                <div class="col">
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <a href="#" class="p-2">
-                                <div style="width: 30px;height: 30px;background: #fff;border-radius: 100%;display: flex;justify-content: center;">
-                                    <i style="margin-top:6px" class="fa fa-facebook"></i>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="p-2">
-                                <div style="width: 30px;height: 30px;background: #fff;border-radius: 100%;display: flex;justify-content: center;">
-                                    <i style="margin-top:6px" class="fa fa-twitter"></i>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="p-2">
-                                <div style="width: 30px;height: 30px;background: #fff;border-radius: 100%;display: flex;justify-content: center;">
-                                    <i style="margin-top:6px" class="fa fa-instagram"></i>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="p-2">
-                                <div style="width: 30px;height: 30px;background: #fff;border-radius: 100%;display: flex;justify-content: center;">
-                                    <i style="margin-top:6px" class="fa fa-tripadvisor"></i>
-                                </div>
-                            </a>
-                        </li>
+        <div class="row footer_inner justify-content-center">
+            <div class="col-lg-6 text-center">
+                <aside class="f_widget social_widget">
+                    <div class="f_logo">
+                        <img src="assets/img/logo.png" alt="">
+                    </div>
+                    <div class="f_title">
+                        <h4>Follow Me</h4>
+                    </div>
+                    <ul class="list">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
                     </ul>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div style="display: grid;justify-content: center;align-content: center;">
-                    <img src="<?= $logoTwo ?>" alt="Logo Footer" style="width:110px">
-                </div>
-                <div style="display: grid;justify-content: center;align-content: center;">
-                    <font style="color: #fff;">
-                        <?= $app ?>
-                    </font>
+                </aside>
+                <div class="copyright">
+                    <p>
+                        Copyright &copy;
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        All rights reserved | This template is made with
+                        <i class="fa fa-heart-o" aria-hidden="true"></i>
+                        by
+                        <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </footer>
-<div id="pb_loader" class="show fullscreen">
-    <?= $loader ?>
-</div>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/slick.min.js"></script>
-<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
-<script src="assets/js/jquery.waypoints.min.js"></script>
-<script src="assets/js/jquery.easing.1.3.js"></script>
+<script src="assets/js/stellar.js"></script>
 <script src="assets/js/jquery.magnific-popup.min.js"></script>
-<script src="assets/js/magnific-popup-options.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-<script src="assets/js/google-map.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="assets/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+<script src="assets/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+<script src="assets/vendors/isotope/isotope-min.js"></script>
+<script src="assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="assets/js/mail-script.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+<script src="assets/js/gmaps.min.js"></script>
+<script src="assets/js/theme.js"></script>
 </body>
 
 </html>
--- include/head.php
+++ include/head.php
@@ -1,30 +1,27 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?= $title; ?></title>
     <link rel="icon" type="image/png" sizes="32x32" href="<?= $icon ?>">
-    <meta name="description" content="Tanpa, Tapi, Tanpa Tapi, TanpaTapi, Warung Makan, Restaurant, Restauran, Makan, Makanan">
-    <meta name="keywords" content="Tanpa, Tapi, Tanpa Tapi, TanpaTapi, Warung Makan, Restaurant, Restauran, Makan, Makanan">
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600|Montserrat:200,300,400, 600, 700" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons/css/ionicons.min.css">
+    <!-- <link rel="icon" href="<?= $icon ?>" type="image/png"> -->
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/vendors/linericon/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/slick.css">
-    <link rel="stylesheet" href="assets/css/helpers.css">
+    <link rel="stylesheet" href="assets/vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="assets/vendors/flaticon/flaticon.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/restaurant.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 </head>
+
 <style>
     body {
         font-family: "Montserrat", Arial, sans-serif;
     }
 </style>
 
-<body data-spy="scroll" data-target="#pb-navbar" data-offset="200" class="mb">
-    <div class="probootstrap-loader"></div>
+<body>
